64df04096817c41ef38223d33bb8447d9c358157
Trevbo25 <trevorbrown25@gmail.com>
2017-12-15 13:51:14 -0700

    presentation ready

--- server/server.js
+++ server/server.js
@@ -1,6 +1,8 @@
 const express = require('express'),
       bodyParser = require('body-parser'),
       cors = require('cors'),
+      
+      // A new dependency. socket.io integrates with the ExpressJS Server.
       socket = require('socket.io');
 
 const app = express();
@@ -11,35 +13,50 @@ messages = [];
 
 const PORT = 3535;
 
+// We initialize a new instance of socket.io by passing in the "server"
+// We are still listening for regular server communication but we are also listening for sockets as well
 const io = socket(app.listen(PORT, () => console.log(`We be listnin on port ${PORT} mon.`)));
+
 
 io.on('connection', onConnect)
 
 function onConnect(socket){
+
     socket.join('chat room')
     console.log('A user joined the chatroom')
     
-    socket.on('sendMessage', message => {
-        // console.log('new message ', message);
-        messages.push(message);
-        // console.log('new array of messages', messages)
-        io.in('chat room').emit('getMessage', messages)
-    })
+    // Everyone including the sender
 
-    socket.on('roomChange', room => {
-        // console.log(room)
+    // socket.on('sendMessage', message => {
+    //     // console.log('new message ', message);
+    //     messages.push(message);
+    //     // console.log('new array of messages', messages)
+    //     io.in('chat room').emit('getMessage', messages)
+    // })
+
+    //
+    
+    // Gets the messages on connection
+
+    socket.on('join', () => {
+        console.log('room')
         socket.emit('getMessage', messages)
     })
 
-    socket.on('typing', name => {
-        // console.log(name)
-        socket.broadcast.emit('newTyper', name)
-    })
 
-    socket.on('stopTyping', name => {
-        // console.log(name + ' stopped typing')
-        socket.broadcast.emit('oldTyper', name)
-    })
+    // Everyone but the sender
+
+    // socket.on('typing', name => {
+    //     // console.log(name)
+    //     socket.broadcast.emit('newTyper', name)
+    // })
+
+    // socket.on('stopTyping', name => {
+    //     // console.log(name + ' stopped typing')
+    //     socket.broadcast.emit('oldTyper', name)
+    // })
+
+    //
 
     socket.on('disconnect', () => {
         console.log('A user disconnected')
--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
 
+// socket.io-client is added when you add the socket.io dependency. It is the client library that loads on the browser side.
 import io from 'socket.io-client';
 
+// There is no specific direction when creating the global socket, it defaults to trying to connect to the host that serves the page AKA the server
 const socket = io();
 
 class App extends Component {
@@ -16,8 +18,10 @@ class App extends Component {
       messagesList: [],
       typers: [],
     }
+
     this.typing = false;
     this.lastTyping = 0;
+
     this.send = this.send.bind(this);
     this.isTyping = this.isTyping.bind(this);
     this.isNotTyping = this.isNotTyping.bind(this);
@@ -25,62 +29,82 @@ class App extends Component {
   }
 
   componentDidMount(){
-    socket.emit('roomChange', this.state.room)
-    socket.on('getMessage', messages => {
-      this.setState({
-        messagesList: messages
-      })
-    });
-    socket.on('newTyper', name => {
-        let newTypers = this.state.typers
-        newTypers.push(name)
-        this.setState({
-          typers: newTypers
-        })
-    });
-    socket.on('oldTyper', name => {
-      var newerTypers = this.state.typers;
-      newerTypers.forEach((e, i, a) => {
-        if(e === name){
-          a.splice(i,1)
-        }
-      })
-      this.setState({
-        typers: newerTypers
-      })
-    })
+
+    // Gets the messages on connection
+
+    socket.emit('join')
+
+    // Everyone including the sender
+
+    // socket.on('getMessage', messages => {
+    //   this.setState({
+    //     messagesList: messages
+    //   })
+    // });
+
+    // 
+
+
+
+
+
+    // Everyone but the sender
+
+    // socket.on('newTyper', name => {
+    //     let newTypers = this.state.typers
+    //     newTypers.push(name)
+    //     this.setState({
+    //       typers: newTypers
+    //     })
+    // });
+
+    // socket.on('oldTyper', name => {
+    //   var newerTypers = this.state.typers;
+    //   newerTypers.forEach((e, i, a) => {
+    //     if(e === name){
+    //       a.splice(i,1)
+    //     }
+    //   })
+    //   this.setState({
+    //     typers: newerTypers
+    //   })
+    // });
     
+    //
+
   }
 
   send(){
-    socket.emit('sendMessage', {
-      name: this.state.name,
-      body: this.state.body,
-      room: this.state.room
-    })
-    this.setState({
-      body: ''
-    })
+
+    // Everyone including the sender
+
+    // socket.emit('sendMessage', {
+    //   name: this.state.name,
+    //   body: this.state.body,
+    //   room: this.state.room
+    // })
+    // this.setState({
+    //   body: ''
+    // })
+
+    //
+
   }
 
   changeRoom(num){
     this.setState({
       room: num
-    }, this.roomChange)
-  }
-
-  roomChange(){
-    socket.emit('roomChange', this.state.room)
+    })
   }
 
   isTyping(){
     this.typing = true
-    socket.emit('typing', this.state.name)
+    // socket.emit('typing', this.state.name)
   }
 
   isNotTyping(){
     this.typing = false
-    socket.emit('stopTyping', this.state.name)
+    // socket.emit('stopTyping', this.state.name)
   }
 
   updateTyping(){
@@ -88,7 +112,6 @@ class App extends Component {
       this.isTyping()
     }
     this.lastTyping = (new Date()).getTime();
-
     setTimeout(() => {
       var newTyping = (new Date()).getTime();
       var timeDiff = newTyping - this.lastTyping;
@@ -97,6 +120,8 @@ class App extends Component {
       }
     }, 500)
   }
+
+
 
   render() {
     const messageList = this.state.messagesList.map((e,i) => {
@@ -119,6 +144,7 @@ class App extends Component {
         <h5 className="typer" key={i}>{e} is typing...</h5>
       )
     })
+
 
     return(
       <div className="papa">
